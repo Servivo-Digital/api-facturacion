0b6c907083de91c21e2a20af6660a27346e89187
Alvaro castillo <6666alvaro666@gmail.com>
2021-07-06 20:29:19 -0500

    refactory  UserFound and authcontroller

--- src/controller/auth.controller.js
+++ src/controller/auth.controller.js
@@ -2,62 +2,111 @@ import jwt from "jsonwebtoken";
 import User from "../models/User";
 import config from "../config";
 import Role from "../models/Roles";
-import { mailValidation } from "../libs/UserFound";
+import {
+  isEmail,
+  isRegistered,
+  consoleMessage,
+  uniqueUsername,
+} from "../libs/UserFound";
 
 export const signUp = async (req, res) => {
   const { username, email, password, roles } = req.body;
 
-  const mailStatus = await mailValidation(email);
+  const mailValid = await isEmail(email);
 
-  if (mailStatus === "Email not validate") {
-    res.status(400).json({ message: "email not validate" });
+  if (!mailValid) {
+    res.status(400).json({
+      message:
+        "the registration failed, verifies your information and tries again",
+    });
+    return;
   } else {
-    const newUser = new User({
-      username,
-      email,
-      password: await User.encryptPassword(password),
-    });
-
-    if (roles) {
-      const foundRoles = await Role.find({ name: { $in: roles } });
-      newUser.roles = foundRoles.map((role) => role._id);
+    const accountCreated = await isRegistered(email);
+    if (accountCreated) {
+      res
+        .status(200)
+        .json({ message: "this email is already associated with an account" });
+      consoleMessage("accountCreated", "user already has an account");
+      return;
     } else {
-      const role = await Role.findOne({ name: "user" });
-      newUser.roles = [role._id];
+      const uniqueUser = await uniqueUsername(username);
+
+      if (uniqueUser) {
+        res
+          .status(400)
+          .json({ message: "user already exists please change it" });
+        consoleMessage("accountCreated", "username already has an account");
+      } else {
+        consoleMessage("Sign In", "user being registered");
+        const newUser = new User({
+          username,
+          email,
+          password: await User.encryptPassword(password),
+        });
+
+        if (roles) {
+          const foundRoles = await Role.find({ name: { $in: roles } });
+          newUser.roles = foundRoles.map((role) => role._id);
+        } else {
+          const role = await Role.findOne({ name: "user" });
+          newUser.roles = [role._id];
+        }
+
+        const savedUser = await newUser.save();
+
+        console.log(savedUser);
+
+        const token = jwt.sign({ id: savedUser._id }, config.SECRET, {
+          expiresIn: 86400, //24 horas
+        });
+
+        res.status(200).json({ token });
+      }
     }
-
-    const savedUser = await newUser.save();
-
-    console.log(savedUser);
-
-    const token = jwt.sign({ id: savedUser._id }, config.SECRET, {
-      expiresIn: 86400, //24 horas
-    });
-
-    res.status(200).json({ token });
   }
 };
 
 export const signIn = async (req, res) => {
+  const { email, password } = req.body;
+  const mail = await isRegistered(email);
 
-  const {email, password } = req.body;
-  const mailStatus = await mailValidation(email);
+  if (mail === undefined) {
+    res.status(400).json({
+      message: "the login failed, verifies your information and tries again",
+    });
+    consoleMessage("SignIn", "bad mail");
+  } else {
+    if (!mail) {
+      res
+        .status(400)
+        .json({ message: "this email not already associated with an account" });
+      consoleMessage("notAccountCreated", "user not already has an account");
+    } else {
+      const UserCompleted = await User.findOne({ email }).populate("roles");
 
-  if (mailStatus === "Email not validate") {
-    res.status(400).json({ message: "email not validate" });
-  }else if (!mailStatus){
-    res.status(404).json({ message: "User not found " });
+      const matchPassword = await User.comparePassword(
+        password,
+        UserCompleted.password
+      );
+
+      if (!matchPassword) {
+        return res.status(401).json({ message: "invalid Password " });
+      }
+
+      const token = jwt.sign({ id: UserCompleted._id }, config.SECRET, {
+        expiresIn: 86400, //24 horas
+      });
+      consoleMessage("SignIn", mail);
+
+      res.status(200).json({
+         User :[{
+           "id":UserCompleted._id,
+           "username":UserCompleted.username,
+           "email":UserCompleted.email,
+           "roles":UserCompleted.roles,
+           token,
+         }],
+        });
+    }
   }
-  const UserCompleted = await User.findOne({email}).populate("roles");
-  
-  const matchPassword = await User.comparePassword(password, UserCompleted.password)
-  
-  if(!matchPassword){
-    return res.status(401).json({ message: "invalid Password " });
-  }
-
-  const token = jwt.sign({ id: UserCompleted._id }, config.SECRET, {
-    expiresIn: 86400, //24 horas
-  });
-  res.status(200).json({ token });
 };
--- src/libs/UserFound.js
+++ src/libs/UserFound.js
@@ -1,24 +1,57 @@
 import User from "../models/User";
 
-export const mailValidation = async (email) => {
+export const isEmail = (email) => {
   const mailValidator =
     /^(([^<>()\[\]\\.,;:\s@”]+(\.[^<>()\[\]\\.,;:\s@”]+)*)|(“.+”))@((\[[0–9]{1,3}\.[0–9]{1,3}\.[0–9]{1,3}\.[0–9]{1,3}])|(([a-zA-Z\-0–9]+\.)+[a-zA-Z]{2,}))$/;
 
-  let isRegistered = false;
+  const EmailEvaluation = mailValidator.test(email);
+  let isMail = false;
 
-  const isMail = mailValidator.test(email);
-
-  if (isMail == true) {
-
-    const userFound = await User.findOne({ email });
-    if (userFound === null) {
-      isRegistered = false;
-    } else {
-      isRegistered = true;
-    }
-    return isRegistered;
-  } else {
-    console.log('email no valido')
-    return "Email not validate";
-  }
+  if (EmailEvaluation) isMail = true;
+  consoleMessage("MailValidaror says the mail is", isMail);
+  return isMail;
 };
+
+export const isRegistered = async (email) => {
+  const mail = isEmail(email);
+
+  if (!mail) return;
+
+  const userFound = await User.findOne({ email });
+
+  let isRegistered;
+  if (userFound === null) {
+    isRegistered = false;
+  } else {
+    isRegistered = true;
+  }
+
+  consoleMessage("isRegistered", isRegistered);
+
+  return isRegistered;
+};
+
+
+export const uniqueUsername = async (username) => {
+  
+  const unique = await User.findOne({ username });
+
+  let isUnique;
+  if (unique === null) 
+  {isUnique = false;} 
+  else {isUnique = true;}
+
+  consoleMessage("uniqueUsername",isUnique);
+
+  return isUnique;
+};
+
+
+export const consoleMessage = (NameFunction, message) => {
+  console.log("  ");
+  console.group(`-- ${NameFunction} --`);
+  console.log(`--> ${message}`);
+  console.groupEnd();
+  console.log("  ");
+};
+
